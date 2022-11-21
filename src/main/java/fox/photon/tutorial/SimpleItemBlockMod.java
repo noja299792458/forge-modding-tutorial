4103d2a38ecd5618267b595bfa6a08fd939e9567
Noja299792458 <noja299792458@gmail.com>
2022-11-22 02:20:22 +0900

    error example 1

--- src/main/java/fox/photon/tutorial/SimpleItemBlockMod.java
+++ src/main/java/fox/photon/tutorial/SimpleItemBlockMod.java
@@ -1,9 +1,7 @@
 package fox.photon.tutorial;
 
-import fox.photon.tutorial.block.TutorialBlock;
 import fox.photon.tutorial.item.TutorialItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,23 +12,19 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod(Constants.MOD_ID)
 public class SimpleItemBlockMod {
     public SimpleItemBlockMod() {
+        //step0 : TutorialItemクラスのインスタンスを作成
+        //TutorialItem tutorialItem = new TutorialItem(); //error
+
         //step1 : DeferredRegisterの定義
         DeferredRegister<Item> defRegItems
                 = DeferredRegister.create(ForgeRegistries.ITEMS,Constants.MOD_ID);
-        DeferredRegister<Block> defRegBlocks
-                = DeferredRegister.create(ForgeRegistries.BLOCKS,Constants.MOD_ID);
 
         //step2 : DeferredRegisterにアイテムを登録
-        RegistryObject<Item> tutorialItem
+        RegistryObject<Item> regObjTutorialItem
                 = defRegItems.register("tutorial_item",()->new TutorialItem());
-        RegistryObject<TutorialBlock> tutorialBlock
-                = defRegBlocks.register("tutorial_block",()->new TutorialBlock());
-        RegistryObject<Item> tutorialBlockItem
-                = defRegItems.register("tutorial_block",()->tutorialBlock.get().getBlockItem());
 
         //step3 : DeferredRegisterにModのEventBusを登録
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         defRegItems.register(modEventBus);
-        defRegBlocks.register(modEventBus);
     }
 }
